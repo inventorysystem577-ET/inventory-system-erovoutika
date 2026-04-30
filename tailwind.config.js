8bdd1136e6ce5ec0e0694b9669c1ac78a5a3793e
Z <w305644@gmail.com>
2026-04-30 10:01:32 +0800

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -9,7 +9,6 @@ export default {
     extend: {},
   },
   plugins: [],
-  // Minimal configuration
   corePlugins: {
     preflight: true,
   },
